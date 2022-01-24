65583db3e98f49347bb2eee89a6b1328dea3e8ce
Igor <avt191540@yandex.ru>
2022-01-24 09:46:29 +0300

    Initial commit course2 lesson13

---
+++ src/main/java/pro/sky/course2lesson13/Course2lesson13Application.java
@@ -0,0 +1,13 @@
+package pro.sky.course2lesson13;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Course2lesson13Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Course2lesson13Application.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/course2lesson13/Course2lesson13ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.course2lesson13;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Course2lesson13ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
