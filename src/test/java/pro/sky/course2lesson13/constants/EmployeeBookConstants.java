5b6b8202647ae262e3a4d8f09d716885f84785d7
Igor <avt191540@yandex.ru>
2022-01-26 21:39:49 +0300

    Homework course2 lesson13 is continuation2

--- src/test/java/pro/sky/course2lesson13/constants/EmployeeBookConstants.java
+++ src/test/java/pro/sky/course2lesson13/constants/EmployeeBookConstants.java
@@ -5,24 +5,24 @@ import pro.sky.course2lesson13.Employee;
 public class EmployeeBookConstants {
     public static final Employee EMPLOYEE_WHO_IS_NOT_EXIST =
             new Employee("Vladimir", "Shatrov", 2, 30000);
-    public static final String FIRST_NAME_NOT_EXIST_ADD = "Vladimir";
-    public static final String FIRST_NAME_NOT_EXIST_ADD_WITH_LOWER_CASE = "vladimir";
-    public static final String FIRST_NAME_NOT_EXIST_ADD_WITH_ILLEGAL_CHARACTERS = "Vladimir123";
-    public static final String LAST_NAME_NOT_EXIST_ADD = "Shatrov";
-    public static final String LAST_NAME_NOT_EXIST_ADD_WITH_LOWER_CASE = "shatrov";
-    public static final String LAST_NAME_NOT_EXIST_ADD_WITH_ILLEGAL_CHARACTERS = "Shatrov456";
-    public static final int DEPARTMENT_ADD = 2;
-    public static final int SALARY_ADD = 30000;
-//    public static final Employee EMPLOYEE_WHO_IS_NOT_EXIST_WITH_LOWER_CASE_NAME =
-//            new Employee("vladimir", "shatrov", 2, 30000);
-//    public static final Employee EMPLOYEE_WHO_IS_NOT_EXIST_WITH_ILLEGAL_CHARACTERS_NAME =
-//            new Employee("vladimir123", "shatrov", 2, 30000);
+    public static final String FIRST_NAME_NOT_EXIST = "Vladimir";
+    public static final String FIRST_NAME_NOT_EXIST_WITH_LOWER_CASE = "vladimir";
+    public static final String FIRST_NAME_NOT_EXIST_WITH_ILLEGAL_CHARACTERS = "Vladimir123";
+    public static final String LAST_NAME_NOT_EXIST = "Shatrov";
+    public static final String LAST_NAME_NOT_EXIST_WITH_LOWER_CASE = "shatrov";
+    public static final String LAST_NAME_NOT_EXIST_WITH_ILLEGAL_CHARACTERS = "Shatrov456";
+    public static final int DEPARTMENT_NOT_EXISTS = 2;
+    public static final int SALARY_NOT_EXISTS = 30000;
     public static final Employee EMPLOYEE_WHO_IS_ALREADY_EXIST =
             new Employee("Ivan", "Ivanov", 2, 20000);
-    public static final Employee EMPLOYEE_WHO_IS_ALREADY_EXIST_WITH_LOWER_CASE_NAME =
-            new Employee("ivan", "ivanov", 2, 20000);
-    public static final Employee EMPLOYEE_WHO_IS_ALREADY_EXIST_WITH_ILLEGAL_CHARACTERS_NAME =
-            new Employee("Ivan57", "Ivanov", 2, 20000);
+    public static final String FIRST_NAME_ALREADY_EXIST = "Ivan";
+    public static final String FIRST_NAME_ALREADY_EXIST_WITH_LOWER_CASE = "ivan";
+    public static final String FIRST_NAME_ALREADY_EXIST_WITH_ILLEGAL_CHARACTERS = "Ivan5";
+    public static final String LAST_NAME_ALREADY_EXIST = "Ivanov";
+    public static final String LAST_NAME_ALREADY_EXIST_WITH_LOWER_CASE = "ivanov";
+    public static final String LAST_NAME_ALREADY_EXIST_WITH_ILLEGAL_CHARACTERS = "Ivanov7";
+    public static final int DEPARTMENT_ALREADY_EXISTS = 2;
+    public static final int SALARY_ALREADY_EXISTS = 20000;
     public static final Employee EMPLOYEE_WHO_IS_ADDED_TO_BOOK =
             new Employee("Vladimir", "Shatrov", 2, 30000);
     public static final Employee EMPLOYEE_WHO_IS_FOUND_OR_REMOVE_IN_BOOK =
--- src/test/java/pro/sky/course2lesson13/service/EmployeeServiceImplTest.java
+++ src/test/java/pro/sky/course2lesson13/service/EmployeeServiceImplTest.java
@@ -1,12 +1,14 @@
 package pro.sky.course2lesson13.service;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
 import pro.sky.course2lesson13.EmployeeBook;
+import pro.sky.course2lesson13.EmployeeExistException;
+import pro.sky.course2lesson13.EmployeeNotExistException;
+import pro.sky.course2lesson13.InvalidInputData;
 
 import static org.mockito.Mockito.*;
 import static pro.sky.course2lesson13.constants.EmployeeBookConstants.*;
@@ -19,23 +21,119 @@ public class EmployeeServiceImplTest {
 
 @Test
     public void shouldCallEmployeeBookMethodWhenAddEmployeeWhoIsNotExists() {
-        when(employeeBookMock.addEmployee(EMPLOYEE_WHO_IS_NOT_EXIST))
+        when(employeeBookMock.addEmployee(eq(EMPLOYEE_WHO_IS_NOT_EXIST)))
                 .thenReturn(EMPLOYEE_WHO_IS_ADDED_TO_BOOK);
         Assertions.assertEquals(EMPLOYEE_WHO_IS_ADDED_TO_BOOK.toString(),
-                out.addEmployeeToBook(FIRST_NAME_NOT_EXIST_ADD, LAST_NAME_NOT_EXIST_ADD, DEPARTMENT_ADD, SALARY_ADD));
+                out.addEmployeeToBook(FIRST_NAME_NOT_EXIST, LAST_NAME_NOT_EXIST,
+                        DEPARTMENT_NOT_EXISTS, SALARY_NOT_EXISTS));
         verify(employeeBookMock, times(1)).addEmployee(EMPLOYEE_WHO_IS_NOT_EXIST);
     }
     @Test
     public void shouldCallEmployeeBookMethodWhenAddEmployeeWhoIsNotExistsWithLoverCaseName() {
-        when(employeeBookMock.addEmployee(EMPLOYEE_WHO_IS_NOT_EXIST))
+        when(employeeBookMock.addEmployee(eq(EMPLOYEE_WHO_IS_NOT_EXIST)))
                 .thenReturn(EMPLOYEE_WHO_IS_ADDED_TO_BOOK);
         Assertions.assertEquals(EMPLOYEE_WHO_IS_ADDED_TO_BOOK.toString(),
-                out.addEmployeeToBook(FIRST_NAME_NOT_EXIST_ADD_WITH_LOWER_CASE, LAST_NAME_NOT_EXIST_ADD_WITH_LOWER_CASE, DEPARTMENT_ADD, SALARY_ADD));
+                out.addEmployeeToBook(FIRST_NAME_NOT_EXIST_WITH_LOWER_CASE,
+                        LAST_NAME_NOT_EXIST_WITH_LOWER_CASE,
+                        DEPARTMENT_NOT_EXISTS, SALARY_NOT_EXISTS));
         verify(employeeBookMock, times(1)).addEmployee(EMPLOYEE_WHO_IS_NOT_EXIST);
     }
+    @Test
+    public void shouldThrowInvalidInputDataExceptionWhenAddEmployeeWhoIsNotExistsWithIllegalCharacters() {
+        when(employeeBookMock.addEmployee(eq(EMPLOYEE_WHO_IS_NOT_EXIST)))
+                .thenReturn(EMPLOYEE_WHO_IS_ADDED_TO_BOOK);
+        Assertions.assertThrows(InvalidInputData.class, ()->
+                out.addEmployeeToBook(FIRST_NAME_NOT_EXIST_WITH_ILLEGAL_CHARACTERS,
+                        LAST_NAME_NOT_EXIST_WITH_ILLEGAL_CHARACTERS,
+                        DEPARTMENT_NOT_EXISTS, SALARY_NOT_EXISTS));
+        verify(employeeBookMock, never()).addEmployee(EMPLOYEE_WHO_IS_NOT_EXIST);
+    }
+    @Test
+    public void shouldThrowEmployeeExistExceptionWhenAddEmployeeWhoIsAlreadyExists() {
+        when(employeeBookMock.addEmployee(any()))
+                .thenThrow(EmployeeExistException.class);
+        Assertions.assertThrows(EmployeeExistException.class, () ->
+                out.addEmployeeToBook(FIRST_NAME_ALREADY_EXIST, LAST_NAME_ALREADY_EXIST,
+                        DEPARTMENT_ALREADY_EXISTS, SALARY_ALREADY_EXISTS));
+        verify(employeeBookMock, times(1)).addEmployee(EMPLOYEE_WHO_IS_ALREADY_EXIST);
+    }
+    @Test
+    public void shouldCallEmployeeBookMethodWhenFindEmployeeWhoIsAlreadyExists() {
+        when(employeeBookMock.findEmployee(eq(EMPLOYEE_WHO_IS_ALREADY_EXIST)))
+                .thenReturn(EMPLOYEE_WHO_IS_FOUND_OR_REMOVE_IN_BOOK);
+        Assertions.assertEquals(EMPLOYEE_WHO_IS_FOUND_OR_REMOVE_IN_BOOK,
+                out.findEmployeeInBook(FIRST_NAME_ALREADY_EXIST, LAST_NAME_ALREADY_EXIST,
+                        DEPARTMENT_ALREADY_EXISTS, SALARY_ALREADY_EXISTS));
+        verify(employeeBookMock, times(1)).findEmployee(EMPLOYEE_WHO_IS_ALREADY_EXIST);
+    }
+    @Test
+    public void shouldCallEmployeeBookMethodWhenFindEmployeeWhoIsAlreadyExistsWithLoverCaseName() {
+        when(employeeBookMock.findEmployee(eq(EMPLOYEE_WHO_IS_ALREADY_EXIST)))
+                .thenReturn(EMPLOYEE_WHO_IS_FOUND_OR_REMOVE_IN_BOOK);
+        Assertions.assertEquals(EMPLOYEE_WHO_IS_FOUND_OR_REMOVE_IN_BOOK,
+                out.findEmployeeInBook(FIRST_NAME_ALREADY_EXIST_WITH_LOWER_CASE,
+                        LAST_NAME_ALREADY_EXIST_WITH_LOWER_CASE,
+                        DEPARTMENT_ALREADY_EXISTS, SALARY_ALREADY_EXISTS));
+        verify(employeeBookMock, times(1)).findEmployee(EMPLOYEE_WHO_IS_ALREADY_EXIST);
+    }
+    @Test
+    public void shouldThrowInvalidInputDataExceptionWhenFindEmployeeWhoIsAlreadyExistsWithIllegalCharacters() {
+        when(employeeBookMock.findEmployee(eq(EMPLOYEE_WHO_IS_ALREADY_EXIST)))
+                .thenReturn(EMPLOYEE_WHO_IS_FOUND_OR_REMOVE_IN_BOOK);
+        Assertions.assertThrows(InvalidInputData.class, ()->
+                out.findEmployeeInBook(FIRST_NAME_ALREADY_EXIST_WITH_ILLEGAL_CHARACTERS,
+                        LAST_NAME_ALREADY_EXIST_WITH_ILLEGAL_CHARACTERS,
+                        DEPARTMENT_ALREADY_EXISTS, SALARY_ALREADY_EXISTS));
+        verify(employeeBookMock, never()).findEmployee(EMPLOYEE_WHO_IS_ALREADY_EXIST);
+    }
+    @Test
+    public void shouldThrowEmployeeNotExistExceptionWhenFindEmployeeWhoIsNotExists() {
+        when(employeeBookMock.findEmployee(any()))
+                .thenThrow(EmployeeNotExistException.class);
+        Assertions.assertThrows(EmployeeNotExistException.class, () ->
+                out.findEmployeeInBook(FIRST_NAME_NOT_EXIST, LAST_NAME_NOT_EXIST,
+                        DEPARTMENT_NOT_EXISTS, SALARY_NOT_EXISTS));
+        verify(employeeBookMock, times(1)).findEmployee(EMPLOYEE_WHO_IS_NOT_EXIST);
+    }
+    @Test
+    public void shouldCallEmployeeBookMethodWhenRemoveEmployeeWhoIsAlreadyExists() {
+        when(employeeBookMock.removeEmployee(eq(EMPLOYEE_WHO_IS_ALREADY_EXIST)))
+                .thenReturn(EMPLOYEE_WHO_IS_FOUND_OR_REMOVE_IN_BOOK);
+        Assertions.assertEquals(EMPLOYEE_WHO_IS_FOUND_OR_REMOVE_IN_BOOK.toString(),
+                out.removeEmployeeFromBook(FIRST_NAME_ALREADY_EXIST, LAST_NAME_ALREADY_EXIST,
+                        DEPARTMENT_ALREADY_EXISTS, SALARY_ALREADY_EXISTS));
+        verify(employeeBookMock, times(1)).removeEmployee(EMPLOYEE_WHO_IS_ALREADY_EXIST);
+    }
+    @Test
+    public void shouldCallEmployeeBookMethodWhenRemoveEmployeeWhoIsAlreadyExistsWithLoverCaseName() {
+        when(employeeBookMock.removeEmployee(eq(EMPLOYEE_WHO_IS_ALREADY_EXIST)))
+                .thenReturn(EMPLOYEE_WHO_IS_FOUND_OR_REMOVE_IN_BOOK);
+        Assertions.assertEquals(EMPLOYEE_WHO_IS_FOUND_OR_REMOVE_IN_BOOK.toString(),
+                out.removeEmployeeFromBook(FIRST_NAME_ALREADY_EXIST_WITH_LOWER_CASE,
+                        LAST_NAME_ALREADY_EXIST_WITH_LOWER_CASE,
+                        DEPARTMENT_ALREADY_EXISTS, SALARY_ALREADY_EXISTS));
+        verify(employeeBookMock, times(1)).removeEmployee(EMPLOYEE_WHO_IS_ALREADY_EXIST);
+    }
+    @Test
+    public void shouldThrowInvalidInputDataExceptionWhenRemoveEmployeeWhoIsAlreadyExistsWithIllegalCharacters() {
+        when(employeeBookMock.removeEmployee(eq(EMPLOYEE_WHO_IS_ALREADY_EXIST)))
+                .thenReturn(EMPLOYEE_WHO_IS_FOUND_OR_REMOVE_IN_BOOK);
+        Assertions.assertThrows(InvalidInputData.class, ()->
+                out.removeEmployeeFromBook(FIRST_NAME_ALREADY_EXIST_WITH_ILLEGAL_CHARACTERS,
+                        LAST_NAME_ALREADY_EXIST_WITH_ILLEGAL_CHARACTERS,
+                        DEPARTMENT_ALREADY_EXISTS, SALARY_ALREADY_EXISTS));
+        verify(employeeBookMock, never()).removeEmployee(EMPLOYEE_WHO_IS_ALREADY_EXIST);
+    }
+    @Test
+    public void shouldThrowEmployeeNotExistExceptionWhenRemoveEmployeeWhoIsNotExists() {
+        when(employeeBookMock.removeEmployee(any()))
+                .thenThrow(EmployeeNotExistException.class);
+        Assertions.assertThrows(EmployeeNotExistException.class, () ->
+                out.removeEmployeeFromBook(FIRST_NAME_NOT_EXIST, LAST_NAME_NOT_EXIST,
+                        DEPARTMENT_NOT_EXISTS, SALARY_NOT_EXISTS));
+        verify(employeeBookMock, times(1)).removeEmployee(EMPLOYEE_WHO_IS_NOT_EXIST);
+    }
 
-//    @BeforeEach
-//    public void initOut() {
-//        out = new EmployeeServiceImpl(employeeBookMock);
-//    }
+
+
 }
